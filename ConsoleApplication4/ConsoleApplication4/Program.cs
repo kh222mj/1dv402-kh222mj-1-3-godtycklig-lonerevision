4ade3ee3c5db8236e22a9f58114132753a1e34df
kh222mj <kh222mj@lnu.se>
2014-09-24 12:18:45 +0200

    jobbar med uppgift 1-3

--- ConsoleApplication4/ConsoleApplication4/Program.cs
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -11,118 +11,138 @@ namespace ConsoleApplication4
         static void Main(string[] args)
         {
 
-            //Deklaration av variabler
-            int antalLoner;
-            int loop = 0;
+        //Deklaration av variabler
+        int antalLoner = 0;
+        int loop = 0;
+        bool restart = true;
 
-            //Läs in hur många löner som ska matas in
-            
-            
-            try 
-            { 
-            Console.Write("Ange antal löner att mata in: ");
-            antalLoner = ReadInt();
-            Console.WriteLine(); //blank rad
-            
-            }
-            catch
-            {
-                Console.WriteLine("FEL! Skriv in ett heltal med antal löner du vill beräkna.");
-            }
-            
-            int[] arrayLoner = new int[antalLoner];
-            int[] arrayLonerCopy = new int[antalLoner];
+        while (restart)
+        {
 
-            //Läs in lönerna och placera dem i array
-            while (loop < antalLoner)
+            while (true)
             {
-                
-            //Felhantering
                 try
                 {
-                    Console.Write("Ange lön nummer " + (loop + 1) + ": ");
-                    arrayLoner[loop] = ProcessSalaries();
-                    loop++;
-                    
+                    //Läs in hur många löner som ska matas in
+                    Console.Write("Ange antal löner att mata in: ");
+                    antalLoner = ReadInt();
+                    Console.WriteLine(); //blank rad
+
+                    if (antalLoner < 2 || antalLoner > 100)
+                    {
+                        throw new IndexOutOfRangeException();
+                    }
+                    break;
                 }
                 catch
-                {                    
-                    Console.WriteLine("FEL! ");
-                }
-             }         
-                
-
-
-
-                //Grafik
-                Console.WriteLine(); //blank rad
-                Console.WriteLine("-------------------------------");
-
-                //Kopiera array
-                arrayLonerCopy = arrayLoner;
-                Array.Sort(arrayLoner);
-
-                //Medianlön
-                if (antalLoner % 2 == 1)
                 {
-                    int median = antalLoner / 2;
-                    Console.WriteLine(string.Format("Medianlön: {0,17:c0}", arrayLoner[Convert.ToInt32(median)]));
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("FEL! Skriv in rimligt antal löner att beräkna.");
+                    Console.BackgroundColor = ConsoleColor.Black;
                 }
+            }
 
-                else
+            //Deklarera arrayer
+            int[] arrayLoner = new int[antalLoner];
+            int[] arrayLonerClone = new int[antalLoner];
+
+            while (true)
+            {
+                try
                 {
-                    int median1 = antalLoner / 2;
-                    int median2 = (antalLoner / 2) + 1;
-                    int medianLon = arrayLoner[median1] + arrayLoner[median2];
-                    Console.WriteLine(string.Format("Medianlön: {0,17:c0}", medianLon));
+                    //Läs in lönerna och placera dem i array
+                    while (loop < antalLoner)
+                    {
+                        Console.Write("Ange lön nummer " + (loop + 1) + ": ");
+                        arrayLoner[loop] = ProcessSalaries();
+                        loop++;
+                    }
+                    break;
                 }
-
-                //Medellön
-                double average;
-                average = arrayLoner.Average();
-                Console.WriteLine(string.Format("Medellön: {0,18:c0}", average));
-
-                //Lönespridning
-                int spridning;
-                spridning = arrayLoner.Max() - arrayLoner.Min();
-                Console.WriteLine(string.Format("Lönespridning: {0,12:c0}", spridning));
-
-                //Grafik
-                Console.WriteLine("-------------------------------");
-                Console.WriteLine(); //blank rad
-
-                //Lista lönerna
-                int loop2 = 0;
-
-                while (loop2 < antalLoner)
+                catch
                 {
-                    Console.Write("{0} ", arrayLonerCopy[loop2]);
-                    loop2++;
-                    
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("FEL! Skriv in en lön du vill beräkna.");
+                    Console.BackgroundColor = ConsoleColor.Black;
                 }
-            
+            }
+
+
+
+
+
+            //Grafik
+            Console.WriteLine(); //blank rad
+            Console.WriteLine("-------------------------------");
+
+            //Kopiera array
+            arrayLonerClone = (int[])arrayLoner.Clone();
+            Array.Sort(arrayLoner);
+
+            //Medianlön
+            if (antalLoner % 2 == 1)
+            {
+                int median = antalLoner / 2;
+                Console.WriteLine(string.Format("Medianlön: {0,17:c0}", arrayLoner[Convert.ToInt32(median)]));
+            }
+
+            else
+            {
+                int median1 = antalLoner / 2;
+                int median2 = (antalLoner / 2) + 1;
+                int medianLon = arrayLoner[median1] + arrayLoner[median2];
+                Console.WriteLine(string.Format("Medianlön: {0,17:c0}", medianLon));
+            }
+
+            //Medellön
+            double average;
+            average = arrayLoner.Average();
+            Console.WriteLine(string.Format("Medellön: {0,18:c0}", average));
+
+            //Lönespridning
+            int spridning;
+            spridning = arrayLoner.Max() - arrayLoner.Min();
+            Console.WriteLine(string.Format("Lönespridning: {0,13:c0}", spridning));
+
+            //Grafik
+            Console.WriteLine("-------------------------------");
+
+            //Lista lönerna
+            int count = 0;
+            for (int col = 1; col < antalLoner + 1; )
+            {
+                Console.Write(string.Format("{0,8}", arrayLonerClone[count]));
+                count++;
+                if (col % 3 == 0)
+                {
+                    Console.WriteLine();
+                }
+                col++;
+            }
+            Console.WriteLine();//Blank rad    
+
+
+        }
+    }
         
-            }
 
+        //Metoder
+        static int ReadInt() 
+        {
+            int input;
+            input = int.Parse(Console.ReadLine());
 
-            //Metoder
-            static int ReadInt() 
-            {
-                int input;
-                input = int.Parse(Console.ReadLine());
+            return input;
+        }
 
-                return input;
-            }
+        static int ProcessSalaries()
+        {
+            int salaries;
+            salaries = Convert.ToInt32(Console.ReadLine());
 
-            static int ProcessSalaries()
-            {
-                int salaries;
-                salaries = Convert.ToInt32(Console.ReadLine());
-
-                return salaries;
-            }
+            return salaries;
+        }
 
         
      }        
 }  
-
